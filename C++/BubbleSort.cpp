44163d5f57a20bcaea13dd38d9750741bf69b9cc
Tomoki <lilac.tm.0x1728@live.jp>
2015-10-14 13:40:22 +0900

    add C++ Version

---
+++ C++/BubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+#define FILE_NAME ""
+
+void bubbleSort()
+{
+
+}
+
+void isSorted()
+{
+
+}
+
+
+int main()
+{
+    std::cout << "test" << std::endl;
+
+    int n = 10;
+
+
+    std::vector<int> x(n);
+
+    int n = 0;
+    std::for_each(
+        x.begin(),
+        x.end(),
+        [&n](int a){ x[i] = n++;
+            std::cout << }
+    );
+
+
+    return EXIT_SUCCESS;
+}
